d53979ee4704a8a513167d3192444cd93770618b
Michael <michaelmccabe711@gmail.com>
2018-11-23 21:35:21 -0500

    added routing & links, finished tut #18, next commit will include some deletions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DirectoryComponent } from "./directory/directory.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+   },
+   { 
+     path: 'directory', 
+     component: DirectoryComponent 
+    },
+  {
+    path: 'directory/:ninja',
+    component: DirectoryComponent,
+   },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
