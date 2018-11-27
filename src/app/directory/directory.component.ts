a78adc1cad6c62c47fcb3db1cb0e1470c77b92f0
Michael <michaelmccabe711@gmail.com>
2018-11-27 15:14:13 -0500

    Using Firebase API

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoggingService } from '../logging.service';
 import { DataService } from '../data.service';
+declare var firebase: any;
 
 @Component({
   selector: 'app-directory',
@@ -19,8 +20,18 @@ export class DirectoryComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dataService.fetchData().subscribe(
+    // Commenting below out because we would like to use the Firebase API instead
+    // We do this so that we can get data AND post data to our Database
+    /*this.dataService.fetchData().subscribe(
       (data) => this.ninjas = data
-    )};
+    );*/
+    this.fbGetData(); 
+  }
+
+  fbGetData(){
+    firebase.database().ref('/').on('child_added', (snapshot) => {
+      this.ninjas.push(snapshot.val())
+    })
+  }
 
 }
